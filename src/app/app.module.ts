d7e501f836705f1d22d31c07a197a4fc8734c656
sagarmaharjan <shagarmaharjan@gmail.com>
2020-02-05 05:32:18 +0545

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HighlightDirective } from './highlight/highlight.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HighlightDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
