df19ab20fac395c29d133cc318de1f2fdfbb926d
sagarmaharjan <shagarmaharjan@gmail.com>
2020-02-05 05:44:36 +0545

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HighlightDirective } from './highlight/highlight.directive';
+import { BetterHighlightDirective } from './better-highlight.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HighlightDirective ],
+  declarations: [ AppComponent, HelloComponent, HighlightDirective, BetterHighlightDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
